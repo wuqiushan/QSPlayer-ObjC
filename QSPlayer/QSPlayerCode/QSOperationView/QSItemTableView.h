15032c32bdb59683956fa28245f4a0bb459dc0a0
wuqiushan <wuqiushan3@163.com>
2020-06-10 18:40:59 +0800

    1.新增右侧弹出视图

---
+++ QSPlayer/QSPlayerCode/QSOperationView/QSItemTableView.h
@@ -0,0 +1,17 @@
+//
+//  QSItemTableView.h
+//  QSPlayer
+//
+//  Created by wuqiushan on 2020/6/10.
+//  Copyright © 2020 wuqiushan3@163.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface QSItemTableView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ QSPlayer/QSPlayerCode/QSOperationView/QSItemTableView.m
@@ -0,0 +1,21 @@
+//
+//  QSItemTableView.m
+//  QSPlayer
+//
+//  Created by wuqiushan on 2020/6/10.
+//  Copyright © 2020 wuqiushan3@163.com. All rights reserved.
+//
+
+#import "QSItemTableView.h"
+
+@implementation QSItemTableView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
